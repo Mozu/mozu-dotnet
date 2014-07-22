c917a7f2d5eadffff5593106fa4f415c7bf761c8
Sanjay <sanjay.mandadi@gmail.com>
2014-07-22 17:15:07 -0500

    Move to new structure

---
+++ Samples/Mozu.Api.Sample/Orders/OrderHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Forms;
+using Mozu.Api.Contracts.CommerceRuntime.Orders;
+using Mozu.Api.Resources.Commerce;
+using Mozu.Api.Resources.Commerce.Orders;
+
+namespace Mozu.Api.Sample.OrderHandler
+{
+    public partial class Orders : Form
+    {
+        private ApiContext _apiContext;
+        public Orders(ApiContext apiContext)
+        {
+            InitializeComponent();
+            _apiContext = apiContext;
+        }
+
+        private void btnGetOrders_Click(object sender, EventArgs e)
+        {
+            btnGetOrders.Text = "Getting Orders...";
+            var orderResource = new OrderResource(_apiContext);
+            OrderCollection orders = orderResource.GetOrders();
+
+            if (orders != null && orders.Items.Count > 0)
+                dataGridViewOrders.DataSource = orders.Items;
+            btnGetOrders.Text = "Refresh Orders";
+        }
+
+        private void dataGridViewOrders_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnGetOrderByGuid_Click(object sender, EventArgs e)
+        {
+            var orderGuid = txtOrderGuid.Text;
+            if (orderGuid.Length > 10)
+            {
+                var orderResource = new OrderResource(_apiContext);
+                Order order = orderResource.GetOrder(orderGuid);
+
+                if (order != null && order.Items.Count > 0)
+                    dataGridViewOrders.DataSource = order.Items;
+            }
+        }
+
+    }
+}
