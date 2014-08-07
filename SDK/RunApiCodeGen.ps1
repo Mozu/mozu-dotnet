97e0739bdc3e2ddf66849892560bd1e63cdb96fc
Darrell Lowrance <darrell_lowrance@volusion.com>
2014-08-07 16:49:34 -0500

    Update RunApiCodeGen.ps1
    Add -UseBasicParsing

--- SDK/RunApiCodeGen.ps1
+++ SDK/RunApiCodeGen.ps1
@@ -1,9 +1,3 @@
-<#
-Sample runline from Sanjay
-.\codegen -Major 1 -Minor 9 -Env "ci" -GenPath "\\\\dev.volusion.com\\data\\QA\\integrations\\ImportExport" -CodeGenBaseUrl "https://6c42d67b88eb2b6.a.passageway.io" -AppClaim "vbOcoAEuoHXZJPQUrbZA/kh/d95wt8YNIfdAGlmvRrSMpWX6AK3UyBhrrbEj6+TCjgerVxSGfC7x/FzVLDRXrel/sSQdalb7Y+Y+tcIuKnAy96MwSRELA217QrK1DK435I5pEaVJCtSfKqcNanK8R2p5ab2RuvYAfeHwxGvBVHnTempQruRoAjNKsKlKnlG+Ag7sCPKz7lWQhvMjQ09rIpBEqYfZSQ04WV+HYV3OkPEWLswDvcOhwvBsHHW/Qu1RooM10qbPVU4wm+0xmQrrk4pLbhrKepu7VRhlgHdSGnS9hrxnsp84C+VqaXoyREj+XccsF2Z2zcqYxkai+jNaM4i+gecOwhTHmoZVU/ac3tOEE+gHTz3ze5ikPdpIghAX4FkgIqV7gb4iLFGp4Cx8rrD3qu1Y0aVRobLQD2M8lA0sQNqwHjMB0Zq088V8moSSDUaJ95TO/CXh0UUW8EPRSDFK3rhIxsjgmaiZnM5QZR5XtSh8xpxMlA0QOlfQIGqQwO3ng85Y+zEo5l6ljvzEY8hrKT1j5Ro+PSMFRv6rqRnL3CswCs9FCc1+3ANUtxo3" -RunPublisher:$false 
-My local runline
-codegen.ps1 -Major 1 -Minor 9 -Env "ci" -GenPath "\\\\dev.volusion.com\\data\\QA\\integrations\\ImportExport" -CodeGenBaseUrl "http://apicodegen.mozu-ci.com/" -AppClaim "vbOcoAEuoHXZJPQUrbZA/kh/d95wt8YNIfdAGlmvRrSMpWX6AK3UyBhrrbEj6+TCjgerVxSGfC7x/FzVLDRXrel/sSQdalb7Y+Y+tcIuKnAy96MwSRELA217QrK1DK435I5pEaVJCtSfKqcNanK8R2p5ab2RuvYAfeHwxGvBVHnTempQruRoAjNKsKlKnlG+Ag7sCPKz7lWQhvMjQ09rIpBEqYfZSQ04WV+HYV3OkPEWLswDvcOhwvBsHHW/Qu1RooM10qbPVU4wm+0xmQrrk4pLbhrKepu7VRhlgHdSGnS9hrxnsp84C+VqaXoyREj+XccsF2Z2zcqYxkai+jNaM4i+gecOwhTHmoZVU/ac3tOEE+gHTz3ze5ikPdpIghAX4FkgIqV7gb4iLFGp4Cx8rrD3qu1Y0aVRobLQD2M8lA0sQNqwHjMB0Zq088V8moSSDUaJ95TO/CXh0UUW8EPRSDFK3rhIxsjgmaiZnM5QZR5XtSh8xpxMlA0QOlfQIGqQwO3ng85Y+zEo5l6ljvzEY8hrKT1j5Ro+PSMFRv6rqRnL3CswCs9FCc1+3ANUtxo3" -RunPublisher:$false 
-#>
 
 param(
 [int]$Major=$(throw "Major param is required"),
@@ -52,7 +46,7 @@ $CodeGenServer = "$CodeGenBaseUrl/Mozu.ApiCodeGen.WebApi"
 
 
 try {
-    $response = Invoke-WebRequest -Method Get -Uri "$CodeGenServer/sdkcodegen/$env/versions"  -ContentType "application/json" # -Proxy "http://localhost:8888"
+    $response = Invoke-WebRequest -Method Get -Uri "$CodeGenServer/sdkcodegen/$env/versions"  -ContentType "application/json" -UseBasicParsing
 
     Write-Host $response
     Write-Host $response.Headers
@@ -80,7 +74,7 @@ Write-Host $body
 
 try 
 {
-    $response = Invoke-WebRequest -Method Post -Uri "$CodeGenServer/sdkcodegen" -Body $body -Headers $headers -ContentType "application/json" -ErrorAction Stop
+    $response = Invoke-WebRequest -Method Post -Uri "$CodeGenServer/sdkcodegen" -Body $body -Headers $headers -ContentType "application/json" -ErrorAction Stop -UseBasicParsing
     Write-Host $response.StatusCode
     Write-Host $response.Headers
 }
