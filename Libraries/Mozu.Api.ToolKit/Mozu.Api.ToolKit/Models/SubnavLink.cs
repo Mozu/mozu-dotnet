82487be194b63e854e996d6a7dc804154038b559
Sanjay Mandadi <sanjay_mandadi@volusion.com>
2014-11-11 11:22:18 -0600

    Change parentId to enum

--- Libraries/Mozu.Api.ToolKit/Mozu.Api.ToolKit/Models/SubnavLink.cs
+++ Libraries/Mozu.Api.ToolKit/Mozu.Api.ToolKit/Models/SubnavLink.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Mozu.Api.ToolKit.Converters;
+using Mozu.Api.ToolKit.Handlers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace Mozu.Api.ToolKit.Models
 {
     public class SubnavLink
     {
-        public string ParentId { get; set; }
+        [JsonConverter(typeof(EnumLowerCaseConverter))]
+        public Parent ParentId { get; set; }
         public string[] Path { get; set; }
         public string Href { get; set; }
         public string AppId { get; set; }
