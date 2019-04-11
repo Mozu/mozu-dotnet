56dd5178294b240bf9eac6ba65f8003b1b72334f
Rakesh Dontula <rakesh.dontula@kibocommerce.com>
2019-04-11 11:50:50 -0500

    fix build

--- Mozu.Api/Contracts/Location/LocationAttribute.cs
+++ Mozu.Api/Contracts/Location/LocationAttribute.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-using System;
+
 using System.Collections.Generic;
 
 using Mozu.Api.Contracts.Core;
