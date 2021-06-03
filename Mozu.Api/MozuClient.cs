1a4c06238fe00af664efb630f57c34dccf2911d8
Raghavendra Patlola <Raghavendra.Patlola@kibocommerce.com>
2021-06-03 11:43:45 -0500

    COM-3916 : Changes to implement api throttling.

--- Mozu.Api/MozuClient.cs
+++ Mozu.Api/MozuClient.cs
@@ -275,7 +275,15 @@ namespace Mozu.Api
 		    if (HttpResponse.StatusCode == HttpStatusCode.NotFound && !MozuConfig.ThrowExceptionOn404)
 		      return default(TResult);
 
-			if (typeof(TResult) == typeof(Stream))
+            if (HttpResponse.StatusCode == (HttpStatusCode)429)
+            {
+                _log.Info($"Too many requests to the API.The thread will have to wait for {HttpResponse.Headers.RetryAfter.Delta.Value.TotalMinutes} minutes to make another API call");
+                Thread.Sleep(HttpResponse.Headers.RetryAfter.Delta.Value);
+                _log.Info($"Retrying API request");
+                await ExecuteRequestAsync(new CancellationToken());
+            }
+
+            if (typeof(TResult) == typeof(Stream))
 				return (TResult)(object)(await HttpResponse.Content.ReadAsStreamAsync().ConfigureAwait(false));
 
 			var stringContent = await HttpResponse.Content.ReadAsStringAsync().ConfigureAwait(false);
