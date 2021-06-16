55694ab0c2ab8389f4c35cd7e48f4678348509fe
Rakesh Dontula <Rakesh.Dontula@kibocommerce.com>
2021-06-16 10:52:33 -0500

    Added Shimpment Data APIs

---
+++ Mozu.Api/Resources/Commerce/Fulfillment/ShipmentDataResource.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mozu.Api.Resources.Commerce.Fulfillment
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class ShipmentDataResource
+    {
+
+		///
+		/// <see cref="Mozu.Api.ApiContext"/>
+		///
+		private readonly IApiContext _apiContext;
+
+
+		public ShipmentDataResource(IApiContext apiContext)
+		{
+			_apiContext = apiContext;
+		}
+
+		public ShipmentDataResource CloneWithApiContext(Action<IApiContext> contextModification)
+		{
+			return new ShipmentDataResource(_apiContext.CloneWith(contextModification));
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="shipmentNumber"></param>
+		/// <param name="ct"></param>
+		/// <returns></returns>
+		public virtual async Task<Dictionary<string, object>> GetShipmentDataAsync(int shipmentNumber,  CancellationToken ct = default(CancellationToken))
+		{
+			MozuClient<Dictionary<string, object>> response;
+			var client = Mozu.Api.Clients.Commerce.Fulfillment.ShipmentDataClient.GetShipmentDataClient(shipmentNumber);
+			client.WithContext(_apiContext);
+			response = await client.ExecuteAsync(ct).ConfigureAwait(false);
+			return await response.ResultAsync();
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="shipmentNumber"></param>
+		/// <param name="ct"></param>
+		/// <returns></returns>
+		public virtual async Task<Dictionary<string, object>> ReplaceShipmentDataAsync(int shipmentNumber, Dictionary<string, object> data, CancellationToken ct = default(CancellationToken))
+		{
+			MozuClient<Dictionary<string, object>> response;
+			var client = Mozu.Api.Clients.Commerce.Fulfillment.ShipmentDataClient.ReplaceShipmentDataClient(shipmentNumber, data);
+			client.WithContext(_apiContext);
+			response = await client.ExecuteAsync(ct).ConfigureAwait(false);
+			return await response.ResultAsync();
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="shipmentNumber"></param>
+		/// <param name="ct"></param>
+		/// <returns></returns>
+		public virtual async Task<System.IO.Stream> DeleteShipmentDataAsync(int shipmentNumber, CancellationToken ct = default(CancellationToken))
+		{
+			MozuClient<System.IO.Stream> response;
+			var client = Mozu.Api.Clients.Commerce.Fulfillment.ShipmentDataClient.DeleteShipmentDataClient(shipmentNumber);
+			client.WithContext(_apiContext);
+			response = await client.ExecuteAsync(ct).ConfigureAwait(false);
+			return await response.ResultAsync();
+		}
+
+
+
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="shipmentNumber"></param>
+		/// <param name="key"></param>
+		/// <param name="ct"></param>
+		/// <returns></returns>
+		public virtual async Task<System.IO.Stream> DeleteShipmentDataByKeyAsync(int shipmentNumber, string key, CancellationToken ct = default(CancellationToken))
+		{
+			MozuClient<System.IO.Stream> response;
+			var client = Mozu.Api.Clients.Commerce.Fulfillment.ShipmentDataClient.DeleteShipmentDataByKeyClient(shipmentNumber, key);
+			client.WithContext(_apiContext);
+			response = await client.ExecuteAsync(ct).ConfigureAwait(false);
+			return await response.ResultAsync();
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="shipmentNumber"></param>
+		/// <param name="lineId"></param>
+		/// <param name="ct"></param>
+		/// <returns></returns>
+		public virtual async Task<Dictionary<string, object>> GetShipmentItemDataAsync(int shipmentNumber, int lineId, CancellationToken ct = default(CancellationToken))
+		{
+			MozuClient<Dictionary<string, object>> response;
+			var client = Mozu.Api.Clients.Commerce.Fulfillment.ShipmentDataClient.GetShipmentItemDataClient(shipmentNumber, lineId);
+			client.WithContext(_apiContext);
+			response = await client.ExecuteAsync(ct).ConfigureAwait(false);
+			return await response.ResultAsync();
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="shipmentNumber"></param>
+		/// <param name="lineId"></param>
+		/// <param name="data"></param>
+		/// <param name="ct"></param>
+		/// <returns></returns>
+		public virtual async Task<Dictionary<string, object>> ReplaceShipmentItemDataAsync(int shipmentNumber,int lineId, Dictionary<string, object> data, CancellationToken ct = default(CancellationToken))
+		{
+			MozuClient<Dictionary<string, object>> response;
+			var client = Mozu.Api.Clients.Commerce.Fulfillment.ShipmentDataClient.ReplaceShipmentItemDataClient(shipmentNumber,lineId, data);
+			client.WithContext(_apiContext);
+			response = await client.ExecuteAsync(ct).ConfigureAwait(false);
+			return await response.ResultAsync();
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="shipmentNumber"></param>
+		/// <param name="lineId"></param>
+		/// <param name="ct"></param>
+		/// <returns></returns>
+		public virtual async Task<System.IO.Stream> DeleteShipmentItemDataAsync(int shipmentNumber, int lineId, CancellationToken ct = default(CancellationToken))
+		{
+			MozuClient<System.IO.Stream> response;
+			var client = Mozu.Api.Clients.Commerce.Fulfillment.ShipmentDataClient.DeleteShipmentItemDataClient(shipmentNumber, lineId);
+			client.WithContext(_apiContext);
+			response = await client.ExecuteAsync(ct).ConfigureAwait(false);
+			return await response.ResultAsync();
+		}
+
+
+
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="shipmentNumber"></param>
+		/// <param name="lineId"></param>
+		/// <param name="key"></param>
+		/// <param name="ct"></param>
+		/// <returns></returns>
+		public virtual async Task<System.IO.Stream> DeleteShipmentItemDataByKeyAsync(int shipmentNumber,int lineId, string key, CancellationToken ct = default(CancellationToken))
+		{
+			MozuClient<System.IO.Stream> response;
+			var client = Mozu.Api.Clients.Commerce.Fulfillment.ShipmentDataClient.DeleteShipmentItemDataByKeyClient(shipmentNumber, lineId, key);
+			client.WithContext(_apiContext);
+			response = await client.ExecuteAsync(ct).ConfigureAwait(false);
+			return await response.ResultAsync();
+		}
+	}
+}
---
+++ Mozu.Api/Urls/Commerce/Fulfillment/ShipmentDataUrl.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mozu.Api.Urls.Commerce.Fulfillment
+{
+	/// <summary>
+	/// 
+	/// </summary>
+    public partial class ShipmentDataUrl
+    {
+		/// <summary>
+		/// 
+		/// </summary>		
+		/// <param name="shipmentNumber"></param>		
+		/// <returns></returns>
+		public static MozuUrl GetShipmentDataUrl(int shipmentNumber)
+		{
+			var url = "/api/commerce/shipments/{shipmentNumber}/data";
+			var mozuUrl = new MozuUrl(url, MozuUrl.UrlLocation.TENANT_POD, false);
+			mozuUrl.FormatUrl("shipmentNumber", shipmentNumber);			
+			return mozuUrl;
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>		
+		/// <param name="shipmentNumber"></param>		
+		/// <returns></returns>
+		public static MozuUrl ReplaceShipmentDataUrl(int shipmentNumber)
+		{
+			var url = "/api/commerce/shipments/{shipmentNumber}/data";
+			var mozuUrl = new MozuUrl(url, MozuUrl.UrlLocation.TENANT_POD, false);
+			mozuUrl.FormatUrl("shipmentNumber", shipmentNumber);
+			return mozuUrl;
+		}
+
+
+		/// <summary>
+		/// 
+		/// </summary>		
+		/// <param name="shipmentNumber"></param>		
+		/// <returns></returns>
+		public static MozuUrl DeleteShipmentDataUrl(int shipmentNumber)
+		{
+			var url = "/api/commerce/shipments/{shipmentNumber}/data";
+			var mozuUrl = new MozuUrl(url, MozuUrl.UrlLocation.TENANT_POD, false);
+			mozuUrl.FormatUrl("shipmentNumber", shipmentNumber);
+			return mozuUrl;
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>		
+		/// <param name="shipmentNumber"></param>
+		/// <param name="key"></param>		
+		/// <returns></returns>
+		public static MozuUrl DeleteShipmentDataByKeyUrl(int shipmentNumber, string key)
+		{
+			var url = "/api/commerce/shipments/{shipmentNumber}/data/{key}";
+			var mozuUrl = new MozuUrl(url, MozuUrl.UrlLocation.TENANT_POD, false);
+			mozuUrl.FormatUrl("shipmentNumber", shipmentNumber);
+			mozuUrl.FormatUrl("key", key);
+			return mozuUrl;
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>		
+		/// <param name="shipmentNumber"></param>
+		/// <param name="lineId"></param>		
+		/// <returns></returns>
+		public static MozuUrl GetShipmentItemDataUrl(int shipmentNumber, int lineId)
+		{
+			var url = "/api/commerce/shipments/{shipmentNumber}/items/{lineId}/data";
+			var mozuUrl = new MozuUrl(url, MozuUrl.UrlLocation.TENANT_POD, false);
+			mozuUrl.FormatUrl("shipmentNumber", shipmentNumber);
+			mozuUrl.FormatUrl("lineId", lineId);
+			return mozuUrl;
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>		
+		/// <param name="shipmentNumber"></param>
+		/// <param name="lineId"></param>		
+		/// <returns></returns>
+		public static MozuUrl ReplaceShipmentItemDataUrl(int shipmentNumber, int lineId)
+		{
+			var url = "/api/commerce/shipments/{shipmentNumber}/items/{lineId}/data";
+			var mozuUrl = new MozuUrl(url, MozuUrl.UrlLocation.TENANT_POD, false);
+			mozuUrl.FormatUrl("shipmentNumber", shipmentNumber);
+			mozuUrl.FormatUrl("lineId", lineId);
+			return mozuUrl;
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>		
+		/// <param name="shipmentNumber"></param>
+		/// <param name="lineId"></param>		
+		/// <returns></returns>
+		public static MozuUrl DeleteShipmentItemDataUrl(int shipmentNumber, int lineId)
+		{
+			var url = "/api/commerce/shipments/{shipmentNumber}/items/{lineId}/data";
+			var mozuUrl = new MozuUrl(url, MozuUrl.UrlLocation.TENANT_POD, false);
+			mozuUrl.FormatUrl("shipmentNumber", shipmentNumber);
+			mozuUrl.FormatUrl("lineId", lineId);
+			return mozuUrl;
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>		
+		/// <param name="shipmentNumber"></param>
+		/// <param name="lineId"></param>
+		/// <param name="key"></param>		
+		/// <returns></returns>
+		public static MozuUrl DeleteShipmentItemDataByKeyUrl(int shipmentNumber, int lineId, string key)
+		{
+			var url = "/api/commerce/shipments/{shipmentNumber}/items/{lineId}/data/{key}";
+			var mozuUrl = new MozuUrl(url, MozuUrl.UrlLocation.TENANT_POD, false);
+			mozuUrl.FormatUrl("shipmentNumber", shipmentNumber);
+			mozuUrl.FormatUrl("lineId", lineId);
+			mozuUrl.FormatUrl("key", key);
+			return mozuUrl;
+		}
+	}
+}
