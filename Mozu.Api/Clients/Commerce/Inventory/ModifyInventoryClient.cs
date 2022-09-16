eeaf246a95bbb93e44b1254c1f48706671ca64a4
Rakesh Dontula <10108790+rakesh-dontula@users.noreply.github.com>
2022-09-16 15:03:57 -0500

    COM-6341: Added Refresh and Adjust inventory api's to the sdk (#129)

---
+++ Mozu.Api/Clients/Commerce/Inventory/ModifyInventoryClient.cs
@@ -0,0 +1,44 @@
+﻿using Mozu.Api.Contracts.Inventory;
+using System;
+using System.Collections.Generic;
+
+namespace Mozu.Api.Clients.Commerce.Inventory
+{
+    public partial class ModifyInventoryClient
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="refreshRequest"></param>
+        /// <param name="responseFields"></param>
+        /// <returns></returns>
+        public static MozuClient<Mozu.Api.Contracts.Inventory.JobIDResponse> RefreshClient(RefreshRequest refreshRequest, string responseFields = null)
+        {
+            var url = Mozu.Api.Urls.Commerce.Inventory.ModifyInventoryUrl.RefreshUrl(refreshRequest, responseFields);
+            const string verb = "POST";
+            var mozuClient = new MozuClient<Mozu.Api.Contracts.Inventory.JobIDResponse>()
+                .WithVerb(verb).WithResourceUrl(url)
+                .WithBody<Mozu.Api.Contracts.Inventory.RefreshRequest>(refreshRequest);
+
+            return mozuClient;
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="adjustRequest"></param>
+        /// <param name="responseFields"></param>
+        /// <returns></returns>
+        public static MozuClient<Mozu.Api.Contracts.Inventory.JobIDResponse> AdjustClient(AdjustRequest adjustRequest, string responseFields = null)
+        {
+            var url = Mozu.Api.Urls.Commerce.Inventory.ModifyInventoryUrl.AdjustUrl(adjustRequest, responseFields);
+            const string verb = "POST";
+            var mozuClient = new MozuClient<Mozu.Api.Contracts.Inventory.JobIDResponse>()
+                .WithVerb(verb).WithResourceUrl(url)
+                .WithBody<Mozu.Api.Contracts.Inventory.AdjustRequest>(adjustRequest);
+
+            return mozuClient;
+        }
+        
+    }
+}
---
+++ Mozu.Api/Resources/Commerce/Inventory/ModifyInventoryResource.cs
@@ -0,0 +1,69 @@
+﻿using Mozu.Api.Contracts.Inventory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mozu.Api.Resources.Commerce.Inventory
+{
+    public partial class ModifyInventoryResource
+    {
+        private readonly IApiContext _apiContext;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="apiContext"></param>
+        public ModifyInventoryResource(IApiContext apiContext)
+        {
+            _apiContext = apiContext;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="contextModification"></param>
+        /// <returns></returns>
+        public ModifyInventoryResource CloneWithApiContext(Action<IApiContext> contextModification)
+        {
+            return new ModifyInventoryResource(_apiContext.CloneWith(contextModification));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="refreshRequest"></param>
+        /// <param name="responseFields"></param>
+        /// <param name="ct"></param>
+        /// <returns></returns>
+        public virtual async Task<Mozu.Api.Contracts.Inventory.JobIDResponse> RefreshAsync(RefreshRequest refreshRequest, string responseFields = null, CancellationToken ct = default(CancellationToken))
+        {
+            MozuClient<Mozu.Api.Contracts.Inventory.JobIDResponse> response;
+            var client = Mozu.Api.Clients.Commerce.Inventory.ModifyInventoryClient.RefreshClient(refreshRequest, responseFields);
+            client.WithContext(_apiContext);
+            response = await client.ExecuteAsync(ct).ConfigureAwait(false);
+            return await response.ResultAsync();
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="adjustRequest"></param>
+        /// <param name="responseFields"></param>
+        /// <param name="ct"></param>
+        /// <returns></returns>
+        public virtual async Task<Mozu.Api.Contracts.Inventory.JobIDResponse> AdjustAsync(AdjustRequest adjustRequest, string responseFields = null, CancellationToken ct = default(CancellationToken))
+        {
+            MozuClient<Mozu.Api.Contracts.Inventory.JobIDResponse> response;
+            var client = Mozu.Api.Clients.Commerce.Inventory.ModifyInventoryClient.AdjustClient(adjustRequest, responseFields);
+            client.WithContext(_apiContext);
+            response = await client.ExecuteAsync(ct).ConfigureAwait(false);
+            return await response.ResultAsync();
+        }
+        
+        
+        
+    }
+}
