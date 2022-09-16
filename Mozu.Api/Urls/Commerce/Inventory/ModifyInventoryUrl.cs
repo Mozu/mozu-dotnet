be010d966b0e8cfbe122cfab187a2ebb6c934e2c
Rakesh Dontula <Rakesh.Dontula@kibocommerce.com>
2022-09-16 12:37:33 -0500

    COM-6341: Added Refresh and Adjust inventory api's to the sdk

---
+++ Mozu.Api/Urls/Commerce/Inventory/ModifyInventoryUrl.cs
@@ -0,0 +1,25 @@
+﻿using Mozu.Api.Contracts.Inventory;
+
+namespace Mozu.Api.Urls.Commerce.Inventory
+{
+    public partial class ModifyInventoryUrl
+    {
+        
+        public static MozuUrl RefreshUrl(RefreshRequest refreshRequest, string responseFields = null)
+        {
+            var url = "/api/commerce/inventory/v5/inventory/refresh/?responseFields={responseFields}";
+            var mozuUrl = new MozuUrl(url, MozuUrl.UrlLocation.TENANT_POD, false);			
+            mozuUrl.FormatUrl("responseFields", responseFields);
+            return mozuUrl;
+        }
+        
+        public static MozuUrl AdjustUrl(AdjustRequest adjustRequest, string responseFields = null)
+        {
+            var url = "/api/commerce/inventory/v5/inventory/adjust/?responseFields={responseFields}";
+            var mozuUrl = new MozuUrl(url, MozuUrl.UrlLocation.TENANT_POD, false);			
+            mozuUrl.FormatUrl("responseFields", responseFields);
+            return mozuUrl;
+        }
+        
+    }
+}
